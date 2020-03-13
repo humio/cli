1ce6468060addd0ade3058198d8b662697f287e0
Jestin Woods <jestin@humio.com>
2020-03-13 15:38:58 -0700

    Add method to get the token for the user who is authenticated

--- api/viewer.go
+++ api/viewer.go
@@ -17,3 +17,15 @@ func (c *Viewer) Username() (string, error) {
 	graphqlErr := c.client.Query(&query, nil)
 	return query.Viewer.Username, graphqlErr
 }
+
+// ApiToken fetches the api token for the user who is currently authenticated.
+func (c *Viewer) ApiToken() (string, error) {
+	var query struct {
+		Viewer struct {
+			ApiToken string
+		}
+	}
+
+	graphqlErr := c.client.Query(&query, nil)
+	return query.Viewer.ApiToken, graphqlErr
+}
